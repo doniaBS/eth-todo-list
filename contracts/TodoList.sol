fdfd234555100f39c96328beb23e2584f1cafd3f
donia ben sedrine <bensedrinedonia@gmail.com>
2023-08-23 21:20:44 +0100

    project setup dependencies + first smart contract

---
+++ contracts/TodoList.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.5.0;
+
+contract TodoList {
+   //keep track of numbers of tasks that are in the to do list
+   uint public taskCount = 0;
+}
